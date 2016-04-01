4a79daf6209efdc9eaf285c7973c602a098c2d77
Patricio López <lopezjuripatricio@gmail.com>
2016-04-01 19:44:36 -0300

    add tests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import HelloWorld from './components/helloworld'
+
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <HelloWorld subtitle="Hey! i'm a prop"/>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
+
 ReactDOM.render(<App />, document.getElementById('root'))
