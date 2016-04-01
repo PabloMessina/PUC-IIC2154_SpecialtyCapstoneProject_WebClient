f89acbaa12cb901cc81d7db75d99984657a54405
Patricio López <lopezjuripatricio@gmail.com>
2016-04-01 19:17:46 -0300

    better server code

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
 
-new WebpackDevServer(webpack(config), {
+const settings = {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+  stats: { colors: true },
+}
+
+const server = new WebpackDevServer(webpack(config), settings)
+
+server.listen(3000, 'localhost', (err, result) => {
   if (err) return console.log(err)
   console.log('Listening at http://localhost:3000/')
 })
