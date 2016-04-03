f64fd207e0ea26f5a4307d7fada08d1efe666baf
Patricio López <lopezjuripatricio@gmail.com>
2016-04-03 01:20:02 -0300

    integrate with eslint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 
-import HelloWorld from './components/helloworld'
+import HelloWorld from './components/helloworld';
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <HelloWorld subtitle="Hey! i'm a prop"/>
+        <HelloWorld subtitle="Hey! i'm a prop" />
         <Button>Default</Button>
       </div>
-    )
+    );
   }
 }
--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -1,4 +1,4 @@
-import React, { Component, StyleSheet } from 'react'
+import React, { Component } from 'react';
 
 
 export default class HelloWorld extends Component {
@@ -8,12 +8,16 @@ export default class HelloWorld extends Component {
         <h1>Hello, world.</h1>
         <h2 style={styles.detail}>Subtitle: {this.props.subtitle}</h2>
       </div>
-    )
+    );
   }
 }
+
+HelloWorld.propTypes = {
+  subtitle: React.PropTypes.string,
+};
 
 const styles = {
   detail: {
     color: 'red',
   },
-}
+};
