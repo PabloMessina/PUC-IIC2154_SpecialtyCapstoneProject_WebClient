14d214f3fe3c0b762fc949127b11ccc6ed1f10c7
josembh <josembh90@gmail.com>
2016-04-09 16:27:25 -0300

    Added tags, search and fixed bugs

--- src/components/document-list/index.js
+++ src/components/document-list/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Col, Grid, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Col, Grid, Row, ListGroup, ListGroupItem, Navbar, Button, Input } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 import AtlasThumbnail from './atlas-thumbnail';
-import InfiniteGrid from 'react-infinite-grid';
 // import renderIf from 'render-if';
 
 /**
@@ -32,28 +31,28 @@ export default class Settings extends Component {
           title: 'Atlas 1',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
+          url: 'img/atlas1.jpg',
           title: 'Atlas 2',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
-          title: 'Atlas 2',
+          url: 'img/atlas2.jpg',
+          title: 'Atlas 3',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
-          title: 'Atlas 2',
+          url: 'img/atlas3.jpg',
+          title: 'Atlas 4',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
-          title: 'Atlas 2',
+          url: 'img/atlas4.jpg',
+          title: 'Atlas 5',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
-          title: 'Atlas 2',
+          url: 'img/atlas5.JPG',
+          title: 'Atlas 6',
         },
         {
-          url: 'img/GILROYFICHA.jpg',
-          title: 'Atlas 2',
+          url: 'img/atlas6.jpg',
+          title: 'Atlas 7',
         },
       ],
     };
@@ -62,24 +61,46 @@ export default class Settings extends Component {
   render() {
     const lista = [];
     this.state.documents.forEach((doc, i) => {
-        lista.push(<Col xs={3} md={3}><AtlasThumbnail doument ={doc}></AtlasThumbnail></Col>);
-      if(i % 4 === 0) {
-        lista.push(<div><Col xs={3} md={3}><AtlasThumbnail doument ={doc}></AtlasThumbnail></Col></div>);
+      if (i % 4 === 0) {
+        lista.push(<div><Col xs={2} md={3}><AtlasThumbnail document ={doc}></AtlasThumbnail></Col></div>);
       }
-    })
+      else {
+        lista.push(<Col xs={2} md={3}><AtlasThumbnail document ={doc}></AtlasThumbnail></Col>);
+      }
+    });
     return (
       <div>
-        <Grid>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Navbar.Form pullLeft>
+              <Input type="text" placeholder="Search" />
+              {' '}
+              <Button type="search">Search</Button>
+              </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Col sm={6} md={2}>
+          <h1>Tags</h1>
+          <ListGroup>
+            <ListGroupItem>Anatomia</ListGroupItem>
+            <ListGroupItem>Tag3</ListGroupItem>
+            <ListGroupItem>Tag2</ListGroupItem>
+            <ListGroupItem>Patologia</ListGroupItem>
+            <ListGroupItem>Tag4</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col sm={6} md={10}>
+          <h1>Atlas</h1>
           <Row className="show-grid">
           {lista}
           </Row>
-        </Grid>
+        </Col>
       </div>
     );
   }
-
-
-
 }
 
 Settings.propTypes = {
