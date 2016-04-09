9ffc26ffa92849f4dc7bed214da3b762a86b17c2
fastaburuaga22 <fastaburuaga@uc.cl>
2016-04-09 16:32:27 -0300

    Atlas description

--- src/components/document-list/atlas-thumbnail.js
+++ src/components/document-list/atlas-thumbnail.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Rater from 'react-rater';
 
 export default class AtlasThumbnail extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.box}>
         <img src={this.props.document.url} style={styles.container} />
         <p>{this.props.document.title}</p>
+        <p>{this.props.document.author}</p>
+        <Rater total={5} rating={2} interactive='false'/>
       </div>
     );
   }
@@ -22,5 +25,11 @@ const styles = {
   container: {
     width: 134,
     height: 200,
+  },
+  box: {
+    width: 320,
+    padding: 10,
+    border: 5,
+    margin: 0,
   },
 };
