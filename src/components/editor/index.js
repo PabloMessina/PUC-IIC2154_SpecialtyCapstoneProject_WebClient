45e3cc623d4f575545048fdcefa5fa224cbff837
vdrg <vrdragicevic@gmail.com>
2016-04-10 18:44:24 -0300

    Change react-quill dependency to github.com/vdrg/react-quill in order to get editor's contents in 'Delta' format.

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -20,7 +20,21 @@ export default class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'Hola <b>Value</b>',
+      text: {
+        ops: [
+          { insert: 'El ' },
+          { insert: 'atrio izquierdo', attributes: { bold: true } },
+          { insert: ', tradicionalmente conocido como ' },
+          { insert: ' aurícula izquierda', attributes: { bold: true, italic: true } },
+          { insert: ', es una de las cuatro cavidades del corazón. Recibe sangre oxigenada proveniente de los pulmones y la impulsa a través de la válvula mitral hacia el ventrículo izquierdo, el cual la distribuye a todo el organismo mediante la arteria aorta.' },
+          { insert: '\n\n' },
+          { insert: 'Para su estudio se divide en dos: ' },
+          { insert: 'orejuela o auriculilla', attributes: { color: 'blue' } },
+          { insert: ' y ' },
+          { insert: 'aurícula propiamente', attributes: { color: 'blue' } },
+          { insert: ' dicha; esta clasificación se da debido a su origen embriológico.' },
+        ],
+      },
     };
     this.onTextChange = this.onTextChange.bind(this);
   }
@@ -48,9 +62,7 @@ export default class Editor extends Component {
             items={toolbar}
           />
 
-          <Panel key="editor" ref="editor" className="quill-contents">
-            <div dangerouslySetInnerHTML={{ __html: this.state.text }} />
-          </Panel>
+          <Panel key="editor" ref="editor" className="quill-contents" />
 
         </ReactQuill>
       </div>
