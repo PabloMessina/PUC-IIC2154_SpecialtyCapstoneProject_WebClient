795ecc10b984fdb3d6c77af008abc183b3a88fe2
Patricio López <lopezjuripatricio@gmail.com>
2016-04-22 09:58:30 -0300

    refactor view

--- src/components/course-create/index.js
+++ src/components/course-create/index.js
@@ -13,9 +13,21 @@ import { Button, Input } from 'react-bootstrap';
 
 export default class CourseCreate extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
+  static get propTypes() {
+    return {
+      name: React.PropTypes.string,
+      teachers: React.PropTypes.array,
+      selectedTeachers: React.PropTypes.array,
+      atlases: React.PropTypes.array,
+      students: React.PropTypes.array,
+      selectedStudents: React.PropTypes.array,
+      description: React.PropTypes.string,
+      associatedAtlases: React.PropTypes.array,
+    };
+  }
+
+  static get defaultProps() {
+    return {
       name: '',
       teachers: ['Constanza Halabi',
                  'Vicente Dragicevic',
@@ -30,35 +42,44 @@ export default class CourseCreate extends Component {
       selectedStudents: [],
       description: '',
       associatedAtlases: [],
+    };
+  }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      teachers: this.props.teachers,
+      selectedTeachers: this.props.selectedTeachers,
+      atlases: this.props.atlases,
+      students: this.props.students,
+      selectedStudents: this.props.selectedStudents,
+      description: this.props.description,
+      associatedAtlases: this.props.associatedAtlases,
     };
     this.addStudent = this.addStudent.bind(this);
     this.addTeachers = this.addTeacher.bind(this);
     this.addAtlas = this.addAtlas.bind(this);
+    this.buttonSubmit = this.buttonSubmit.bind(this);
   }
 
   addStudent(student) {
-    // const students = [].concat(this.state.selectedStudents);
-    // students.push(student);
-    // this.setState({selectedStudents: students});
     const selectedStudents = [...this.state.selectedStudents, student];
     this.setState({ selectedStudents });
   }
 
   addTeacher(teacher) {
-    // const students = [].concat(this.state.selectedStudents);
-    // students.push(student);
-    // this.setState({selectedStudents: students});
     const selectedTeachers = [...this.state.selectedTeachers, teacher];
     this.setState({ selectedTeachers });
   }
 
   addAtlas(atlas) {
-    // const students = [].concat(this.state.selectedStudents);
-    // students.push(student);
-    // this.setState({selectedStudents: students});
     const associatedAtlases = [...this.state.associatedAtlases, atlas];
     this.setState({ associatedAtlases });
+  }
+
+  buttonSubmit() {
+    return { prueba: 'hola' };
   }
 
   render() {
@@ -72,38 +93,34 @@ export default class CourseCreate extends Component {
           onChange={e => this.setState({ name: e.target.value })}
         />
         <Input type="select" label="Select Teachers" multiple>
-          {this.state.teachers.map(teacher => {
-            return (
-              <option value={teacher} onClick= {() => this.addTeacher(teacher)}>
+          {this.state.teachers.map(teacher => (
+            <option value={teacher} onClick={() => this.addTeacher(teacher)}>
               {teacher}
-             </option>
-            );
-          })}
+            </option>
+          ))}
         </Input>
         <Input type="select" label="Select Students" multiple >
-          {this.state.students.map(student => {
-            return (
-              <option value={student} onClick= {() => this.addStudent(student)}>
+          {this.state.students.map(student => (
+            <option value={student} onClick={() => this.addStudent(student)}>
               {student}
-             </option>
-            );
-          })}
+            </option>
+          ))}
         </Input>
-        <Input type="textarea" label="Course Description"
+        <Input
+          type="textarea"
+          label="Course Description"
           placeholder="Example: This course
           focuses in the study of the human body..."
           onChange={e => this.setState({ description: e.target.value })}
         />
         <Input type="select" label="Select Asociated Atlases" multiple >
-          {this.state.atlases.map(atlas => {
-            return (
-              <option value={atlas} onClick= {() => this.addAtlas(atlas)}>
+          {this.state.atlases.map(atlas => (
+            <option value={atlas} onClick={() => this.addAtlas(atlas)}>
               {atlas}
-             </option>
-            );
-          })}
+            </option>
+         ))}
         </Input>
-
+        <Button bsStyle="primary" onclick={() => this.buttonSubmit}> Submit Course </Button>
       </div>
     );
   }
