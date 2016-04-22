677440fda54db484d40c5e6627a4924d1e73f9cc
Patricio López <lopezjuripatricio@gmail.com>
2016-04-22 14:34:44 -0300

    show current session

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -6,26 +6,37 @@ import { Grid } from 'react-bootstrap';
 import { user, auth } from '../../app';
 import NavigationBar from '../navigation-bar';
 
+const STATES = {
+  LOADING: 'LOADING',
+  AUTHED: 'AUTHED',
+  ANNON: 'ANNON',
+};
 
 export default class Main extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      state: STATES.LOADING,
+      error: null,
+    };
     this.auth();
   }
 
   auth() {
     return auth().then(() => {
       console.log('Logged as:', user().name);
+      this.setState({ state: STATES.AUTHED });
     }).catch(err => {
       console.log('Auth error:', err);
+      this.setState({ state: STATES.ANNON, error: err });
     });
   }
 
   render() {
     return (
       <div>
-        <NavigationBar title="App" />
+        <NavigationBar title="App" user={user()} />
         <Grid style={styles.content}>
           {this.props.children}
         </Grid>
--- src/components/navigation-bar/index.js
+++ src/components/navigation-bar/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
+
+import { logout } from '../../app';
 
 
 export default class NavigationBar extends Component {
@@ -8,7 +10,48 @@ export default class NavigationBar extends Component {
   static get defaultProps() {
     return {
       title: 'App',
+      user: null,
     };
+  }
+
+  constructor(props) {
+    super(props);
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  onLogout() {
+    return logout().then(() => browserHistory.push('/'));
+  }
+
+  rightNavigation() {
+    if (this.props.user) {
+      return (
+        <Nav pullRight>
+          <NavDropdown eventKey={1} title={this.props.user.name} id="basic-nav-dropdown">
+            <MenuItem eventKey={1.1} onClick={() => browserHistory.push('/profile')}>
+              Profile
+            </MenuItem>
+            <MenuItem eventKey={1.2} onClick={() => browserHistory.push('/settings')}>
+              Settings
+            </MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={1.3} onClick={this.onLogout}>
+              Log out
+            </MenuItem>
+          </NavDropdown>
+        </Nav>
+      );
+    }
+    return (
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#" onClick={() => browserHistory.push('/login')}>
+          Login
+        </NavItem>
+        <NavItem eventKey={2} href="#" onClick={() => browserHistory.push('/signup')}>
+          Sign Up
+        </NavItem>
+      </Nav>
+    );
   }
 
   render() {
@@ -29,18 +72,10 @@ export default class NavigationBar extends Component {
             <NavItem eventKey={2} href="#" onClick={() => browserHistory.push('/courses')}>
               Courses
             </NavItem>
-            <NavItem eventKey={3} onClick={() => browserHistory.push('/settings')}>
-              Configuración
-            </NavItem>
           </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#" onClick={() => browserHistory.push('/login')}>
-              Login
-            </NavItem>
-            <NavItem eventKey={2} href="#" onClick={() => browserHistory.push('/signup')}>
-              Sign Up
-            </NavItem>
-          </Nav>
+
+          {this.rightNavigation()}
+
         </Navbar.Collapse>
 
       </Navbar>
@@ -50,6 +85,7 @@ export default class NavigationBar extends Component {
 
 NavigationBar.propTypes = {
   title: React.PropTypes.node,
+  user: React.PropTypes.any,
 };
 
 const styles = {
