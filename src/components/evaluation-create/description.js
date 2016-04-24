93fda8b31e2a900d86a784c4d90fb070b663f1a0
Patricio López <lopezjuripatricio@gmail.com>
2016-04-24 15:46:54 -0300

    basic form

--- src/components/evaluation-create/description.js
+++ src/components/evaluation-create/description.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Panel,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Alert,
+} from 'react-bootstrap';
 
 
 export default class MinTemplate extends Component {
@@ -7,19 +15,53 @@ export default class MinTemplate extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: '',
+      description: '',
     };
   }
 
   render() {
     return (
-      <Grid style={styles.container}>
+      <div style={styles.container}>
         <Row>
           <Col xs={12}>
-            <p>Hello world</p>
+            <form onSubmit={this.onSubmit}>
+
+              <FormGroup controlId="title">
+                <ControlLabel>Title</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.title}
+                  placeholder="Mid-term evaluation"
+                  label="Title"
+                  onChange={e => this.setState({ title: e.target.value })}
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+
+              <FormGroup controlId="description">
+                <ControlLabel>Description</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  value={this.state.description}
+                  placeholder="Evaluation description..."
+                  onChange={e => this.setState({ description: e.target.value })}
+                />
+              </FormGroup>
+
+              <FormGroup controlId="attendance">
+                <ControlLabel>Attendance</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="none">Not required</option>
+                  <option value="optional">Optional attendance</option>
+                  <option value="obligatory">Obligatory attendance</option>
+                </FormControl>
+              </FormGroup>
+
+            </form>
           </Col>
         </Row>
-      </Grid>
+      </div>
     );
   }
 }
--- src/components/evaluation-create/index.js
+++ src/components/evaluation-create/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, ButtonToolbar, ButtonGroup, Button } from 'react-bootstrap';
+import { Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 
 
