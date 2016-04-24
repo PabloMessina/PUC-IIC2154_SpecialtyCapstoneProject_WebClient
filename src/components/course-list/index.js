05803a02a30417791eec132b97b5260e7c1f8d22
DiegoSteinsapir <dasteinsapir@uc.cl>
2016-04-24 15:59:31 -0300

    make courses list

--- src/components/course-list/index.js
+++ src/components/course-list/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Media } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
 
 
 export default class CourseList extends Component {
@@ -18,24 +19,42 @@ export default class CourseList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      courses: props.courses,
-    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(courseId) {
+    const url = `/courses/show/${courseId}`;
+    return browserHistory.push(url);
   }
 
   render() {
     return (
-      <Grid style={styles.container}>
-        {this.state.courses.map(course => (
-          <div className="jumbotron jumbotron-fluid col-xs-12">
-            <img className="col-xs-3 img-circle" src={course.imageSource} alt="no disp" />
-            <div className="container col-xs-8">
-              <h3 className="display-1">{course.courseName}</h3>
-              <p className="lead">{course.description}</p>
-            </div>
-          </div>
+      <div style={styles.container}>
+      {this.props.courses.map((course, i) => (
+        <Media key={i} style={styles.cell}>
+          <Media.Left>
+            <img
+              style={{ cursor: 'pointer' }}
+              width={70}
+              height={70}
+              onClick={() => this.handleClick(course.id)}
+          /*  src={course.imageSource} */
+              src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-university-assets.s3.amazonaws.com/89/d0ddf06ad611e4b53d95ff03ce5aa7/360px.png"
+              alt="Not available"
+            />
+          </Media.Left>
+          <Media.Body>
+            <Media.Heading
+              style={{ cursor: 'pointer' }}
+              onClick={() => this.handleClick(course.id)}
+            >
+            {course.name}
+            </Media.Heading>
+            <p>{course.description}</p>
+          </Media.Body>
+        </Media>
         ))}
-      </Grid>
+      </div>
     );
   }
 }
@@ -47,5 +66,9 @@ export default class CourseList extends Component {
 const styles = {
   container: {
 
+  },
+  cell: {
+    marginTop: 10,
+    paddingRight: 8,
   },
 };
