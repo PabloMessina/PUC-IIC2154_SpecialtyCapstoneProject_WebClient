f15b879d6266b0be601ee7e9e611cb527ed6ec8d
Patricio López <lopezjuripatricio@gmail.com>
2016-04-26 18:40:14 -0300

    improve course list view

--- src/components/course-list/index.js
+++ src/components/course-list/index.js
@@ -31,28 +31,31 @@ export default class CourseList extends Component {
     return (
       <div style={styles.container}>
       {this.props.courses.map((course, i) => (
-        <Media key={i} style={styles.cell}>
-          <Media.Left>
-            <img
-              style={{ cursor: 'pointer' }}
-              width={70}
-              height={70}
-              onClick={() => this.handleClick(course.id)}
-          /*  src={course.imageSource} */
-              src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-university-assets.s3.amazonaws.com/89/d0ddf06ad611e4b53d95ff03ce5aa7/360px.png"
-              alt="Not available"
-            />
-          </Media.Left>
-          <Media.Body>
-            <Media.Heading
-              style={{ cursor: 'pointer' }}
-              onClick={() => this.handleClick(course.id)}
-            >
-            {course.name}
-            </Media.Heading>
-            <p>{course.description}</p>
-          </Media.Body>
-        </Media>
+        <div>
+          <Media key={i} style={styles.cell}>
+            <Media.Left>
+              <img
+                style={{ cursor: 'pointer' }}
+                width={70}
+                height={70}
+                onClick={() => this.handleClick(course.id)}
+            /*  src={course.imageSource} */
+                src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-university-assets.s3.amazonaws.com/89/d0ddf06ad611e4b53d95ff03ce5aa7/360px.png"
+                alt="Not available"
+              />
+            </Media.Left>
+            <Media.Body>
+              <Media.Heading
+                style={{ cursor: 'pointer' }}
+                onClick={() => this.handleClick(course.id)}
+              >
+              {course.name}
+              </Media.Heading>
+              <p>{course.description}</p>
+            </Media.Body>
+          </Media>
+          <hr />
+        </div>
         ))}
       </div>
     );
@@ -68,7 +71,8 @@ const styles = {
 
   },
   cell: {
-    marginTop: 10,
+    marginTop: 20,
+    marginBottom: 20,
     paddingRight: 8,
   },
 };
