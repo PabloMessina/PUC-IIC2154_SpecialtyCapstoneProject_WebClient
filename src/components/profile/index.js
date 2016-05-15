440ba1dfa5d5f230a8a02d455338827021ada201
Jose Bustamante <josembh90@gmail.com>
2016-05-15 16:53:10 -0400

    now displays the current user and info correctly

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,45 +1,67 @@
 import React, { Component } from 'react';
 import { Row, Col, Image, Nav, NavItem, ListGroup, ListGroupItem, Panel, Glyphicon } from 'react-bootstrap';
 
+import app, { currentUser } from '../../app';
+const participantService = app.service('/participants');
 
 export default class UserProfile extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      user: [],
     };
+    this.fetchUsers = this.fetchUsers.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    const query = {
+      userId: currentUser().id,
+      $populate: 'user',
+    };
+    participantService.find({ query })
+		.then(result => result.data)
+		.then(participantships => {
+  this.setState({ user: participantships });
+			// console.log(participantships);
+			// ...
+		});
+		// .catch(err => console.log(err));
   }
 
   render() {
+    console.log(currentUser());
     return (
       <div style={styles.container}>
         <Row className="show-grid">
           <Col xs={12} md={3}>
             <Image src="http://placehold.it/220x229" circle thumbnail responsive />
-            <h1>John Smith</h1>
-            <h5><Glyphicon glyph="user" /> mrjohnny69</h5>
+            <h1>{currentUser().name}</h1>
+            <h5><Glyphicon glyph="envelope" /> {currentUser().email}</h5>
             <hr />
-            <h4>Student since: 2014</h4>
+            <h4>Member since: {currentUser().createdAt}</h4>
           </Col>
           <Col xs={6} md={9}>
             <Nav bsStyle="tabs" activeKey={1} onSelect={this.handleSelect}>
               <NavItem eventKey={1}><Glyphicon glyph="dashboard" /> My Activity</NavItem>
-              <NavItem eventKey={2}><Glyphicon glyph="education" /> My Courses</NavItem>
             </Nav>
             <br />
             <div>
               <Row>
                 <Col md={6}>
                   <ListGroup>
-                    <ListGroupItem bsStyle="info" style={styles.titles}><Glyphicon glyph="eye-open" /> Most Viewed Atlases</ListGroupItem>
+                    <ListGroupItem bsStyle="info" style={styles.titles}><Glyphicon glyph="heart-empty" /> Favorite Atlases</ListGroupItem>
                     <ListGroupItem>Calculus 1</ListGroupItem>
                     <ListGroupItem>Calculus 2</ListGroupItem>
                   </ListGroup>
                 </Col>
                 <Col md={6}>
                   <ListGroup>
-                    <ListGroupItem bsStyle="info" style={styles.titles}><Glyphicon glyph="heart-empty" /> Favorites</ListGroupItem>
+                    <ListGroupItem bsStyle="info" style={styles.titles}><Glyphicon glyph="education" /> Courses</ListGroupItem>
                     <ListGroupItem>Anatomy</ListGroupItem>
                     <ListGroupItem>Calculus 1</ListGroupItem>
                   </ListGroup>
@@ -48,7 +70,7 @@ export default class UserProfile extends Component {
               <Row>
                 <Col md={12}>
                   <Panel header={"Activity"}>
-                    <h4>mrjohnny69 has no activity during this period.</h4>
+                    <h4>{currentUser().name} has no activity during this period.</h4>
                   </Panel>
                 </Col>
               </Row>
