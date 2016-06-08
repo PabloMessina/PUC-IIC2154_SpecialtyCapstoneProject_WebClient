07f91b8d80f3ab3849b574b2fff48913d3192ac1
Patricio López <lopezjuripatricio@gmail.com>
2016-06-08 11:58:52 -0300

    change production logs format

--- server.production.js
+++ server.production.js
@@ -7,7 +7,7 @@ const path = require('path');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('combined'));
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms')); // eslint-disable-line
 
 // serve static assets normally
 app.use(express.static(`${__dirname}/public`));
