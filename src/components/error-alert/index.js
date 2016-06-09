e3a2516c9c6be6a671901ba2794488fa280ea791
taandrighetti <taandrighetti@uc.cl>
2016-06-09 13:58:52 -0400

    Fix/error alert (#93)

--- src/components/error-alert/index.js
+++ src/components/error-alert/index.js
@@ -15,12 +15,21 @@ export default class ErrorAlert extends Component {
       return null;
     }
 
-    return (
-      <Alert bsStyle="danger" onDismiss={onDismiss} style={styles.alert}>
-        <h4>Oh snap! You got an error!</h4>
-        <p>{error.message}</p>
-      </Alert>
-    );
+    if (error.message) {
+      console.warn('Oh snap! You got an error!'); // eslint-disable-line
+      console.dir(error);   // eslint-disable-line
+      // TODO: NewRelic should log this?
+
+      return (
+        <Alert bsStyle="danger" onDismiss={onDismiss} style={styles.alert}>
+          <h4>Oh snap! You got an error!</h4>
+          <p>{error.message}</p>
+        </Alert>
+      );
+    }
+
+    // console.warn('Someone set "state.error" with something that is not an error!', error);   // eslint-disable-line
+    return null;
   }
 }
 
