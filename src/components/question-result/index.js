ab945a6e51ef56358fe22b785aa276d3ea30431c
Geraldine Monsalve <geralmonsa@gmail.com>
2016-06-16 15:24:55 -0300

    .

---
+++ src/components/question-result/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  // Form,
+  // Button,
+  // FormControl,
+  // ControlLabel,
+  // FormGroup,
+} from 'react-bootstrap';
+
+// import { Colors } from '../../styles';
+
+export default class TrueFalse extends Component {
+
+  static get propTypes() {
+    return {};
+  }
+
+  static get defaultProps() {
+    return {};
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <p>holi</p>
+    );
+  }
+}
+
+const styles = {
+  container: {},
+};
